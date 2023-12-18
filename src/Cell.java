4439ed1fbd3c889ad786531b5c9c660f16dc23ae
Your Name <mawardsantika@gmail.com>
2023-12-18 15:47:02 +0700

    update

--- src/Cell.java
+++ src/Cell.java
@@ -1,28 +1,41 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2023/2024
+ * Group Capstone Project
+ * Group #9
+ * 1 - 5026221131 - Maulina Nur Laila
+ * 2 - 5026221172 - Arya Putra Tsabitah Firjatulloh
+ * 3 - 5026221179 - Kadek Mawar Kumala Dewi
+ */
+
 import java.awt.*;
 import javax.swing.*;
 
 public class Cell {
 
-    public static final int SIZE = 180;
-    public static final int PADDING = SIZE / 3;
-    public static final int SEED_SIZE = SIZE - PADDING * 2;
-    public static final int SEED_STROKE_WIDTH = 8;
+    public static final int SIZE = 180; // cell width/height (square)
     private Image xImage = new ImageIcon("src/angry.png").getImage();
     private Image oImage = new ImageIcon("src/marah.png").getImage();
 
+    // Define properties (package-visible)
+    /** Content of this cell (Seed.EMPTY, Seed.CROSS, or Seed.NOUGHT) */
     Seed content;
+    /** Row and column of this cell */
     int row, col;
 
+    /** Constructor to initialize this cell with the specified row and col */
     public Cell(int row, int col) {
         this.row = row;
         this.col = col;
         content = Seed.NO_SEED;
     }
 
+    /** Reset this cell's content to EMPTY, ready for new game */
     public void newGame() {
         content = Seed.NO_SEED;
     }
 
+    /** Paint itself on the graphics canvas, given the Graphics context */
     public void paint(Graphics g) {
         int x = col * SIZE;
         int y = row * SIZE;
